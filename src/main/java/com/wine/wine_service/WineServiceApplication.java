8c978f7a98b840c63b1be99f809c279aec2b3454
willian.theodoro <theodoro.willianalexandre@gmail.com>
2025-01-13 17:05:37 -0300

    firs commit com estrutura

---
+++ src/main/java/com/wine/wine_service/WineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wine.wine_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wine/wine_service/WineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wine.wine_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
